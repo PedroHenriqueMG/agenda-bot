68493bff2316ae98811d41693e69a9c07159483b
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-11-23 01:15:36 -0300

    feat: get event time of day

--- src/discord/events/reminder.ts
+++ src/discord/events/reminder.ts
@@ -8,21 +8,12 @@ new Event({
 	name: "reminder",
 	event: "ready",
 	async run(client) {
-        cron.schedule("* * * * *", async () => {
+        cron.schedule("0 8 * * *", async () => {
             const now = new Date();
-            now.setHours(0, 0, 0, 0); 
-            const endOfDay = new Date(now);
-            endOfDay.setHours(23, 59, 59, 999); 
+            const day = now.getDate().toString().padStart(2, "0");
            
-
-            const events = await db.event.findMany({
-                where: {
-                    time: {
-                        gte: now,      
-                        lt: endOfDay,  
-                    },
-                }
-            })
+            const allEvents = await db.event.findMany()
+            const events = allEvents.filter(e => new Date(e.time).getDate().toString().padStart(2, "0") === day)
 
             if(!events.length) return
 
@@ -32,7 +23,7 @@ new Event({
                 if(channelId){
                     const channel = client.channels.cache.get(channelId)
         
-                    const date = moment(event.time, "DD/MM/YYYY", true);
+                    const date = moment(new Date(event.time), "DD/MM/YYYY", true);
                     if(channel?.isTextBased && channel.type === 0) {
                         if(event.type === "fixed") {
                            channel?.send(`<@${channelCollection.get(process.env.CHANNEL_KEY)?.userId}> Você tem um evento para hoje!\n**Nome:** ${event.name}\n**Data e Hora:** ${date.format("DD/MM/YYYY [às] HH:mm")}\n**Descrição:** ${event.description}`)
